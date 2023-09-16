ed0719807f58451ac8437090137d297cedc2e30b
ahmed jan <ahmedjan57.rind@gmail.com>
2023-09-16 17:58:51 +0500

    nothing

--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -1,7 +1,15 @@
 const admin = require("../firebase/index");
 
 exports.authCheck =(req, res, next) => {
-    console.log(req.headers);
+    // console.log(req.headers);
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.status(401).json({
+            error: "Invalid User or Expired token"
+        });        
+    }
     next();
 };
 
